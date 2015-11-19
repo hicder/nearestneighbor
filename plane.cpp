8b98c630c97e19d3c0671528656a97ea1c0affe5
Hieu Pham <hieu@Hieus-MacBook-Pro.local>
2015-11-18 19:49:11 -0600

    operator==

--- plane.cpp
+++ plane.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 Plane::Plane(double px, double py) {
-	this->a = 2*px;
-	this->b = 2*py;
-	this->c = -1 * (px*px + py*py);
+  this->a = 2*px;
+  this->b = 2*py;
+  this->c = -1 * (px*px + py*py);
 }
 
 Plane::Plane(double a, double b, double c) {
-	this->a = a;
-	this->b = b;
-	this->c = c;
+  this->a = a;
+  this->b = b;
+  this->c = c;
 }
 
 pair<double, double> Plane::toPoint() {
-	return make_pair(a/2, b/2);
+  return make_pair(a/2, b/2);
+}
+
+bool Plane::operator==(const Plane& other) {
+  return (a == other.a) && (b == other.b) && (c == other.c);
 }
--- plane.h
+++ plane.h
@@ -3,12 +3,13 @@
 #include <utility>
 
 class Plane {
-	public:
-		double a;
-		double b;
-		double c;
-		Plane(double, double); // construct the plane -z = ax + by + c given a 2d point
-		Plane(double, double, double); // construct the plane -z = ax + by + c, given a, b and c
-		std::pair<double, double> toPoint(); // convert the plane into the corresponding point in 2d space
+  public:
+    double a;
+    double b;
+    double c;
+    bool operator==(Plane const&) const;
+    Plane(double, double); // construct the plane -z = ax + by + c given a 2d point
+    Plane(double, double, double); // construct the plane -z = ax + by + c, given a, b and c
+    std::pair<double, double> toPoint(); // convert the plane into the corresponding point in 2d space
 };
 
