d2491b56e95cc34f20ee27a4b6c94ca7ea4c5f73
mehul <mgoyal3@illinois.edu>
2015-11-18 18:39:05 -0600

    added point to plane and plane to point functions

--- plane.cpp
+++ plane.cpp
@@ -1,7 +1,19 @@
 #include "plane.h"
 
+using namespace std;
+
+Plane::Plane(double px, double py) {
+	this->a = 2*px;
+	this->b = 2*py;
+	this->c = -1 * (px*px + py*py);
+}
+
 Plane::Plane(double a, double b, double c) {
 	this->a = a;
 	this->b = b;
 	this->c = c;
+}
+
+pair<double, double> Plane::toPoint() {
+	return make_pair(a/2, b/2);
 }
