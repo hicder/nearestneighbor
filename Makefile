1032c91174a18ee5a556932472a74962cb423cd7
Hieu Pham <hieu@Hieus-MBP.lan>
2015-12-07 11:29:47 -0600

    some test

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = g++
 CFLAGS = -c -g -O0 -Wall -Werror -std=c++11
 LINKER = g++
 TESTLDFLAGS = -L/usr/local/lib -lgtest -lpthread
-TESTCFLAGS = -I/usr/local/include
+TESTCFLAGS = -I/usr/local/include -std=c++11
 
 nn: main.o Plane.o Line.o Subset.o VerticalRayDS.o VerticalRay.o Cutting.o Point.o Utils.o
 	$(LINKER) main.o Plane.o Line.o Subset.o VerticalRayDS.o VerticalRay.o Cutting.o Point.o Utils.o -o nn
@@ -29,6 +29,8 @@ clean:
 
 .PHONY: nn test clean all
 
-test: plane.o
-	$(LINKER) $(TESTCFLAGS) plane.o PlaneTest.cpp -o planetest $(TESTLDFLAGS)
+test: Plane.o Utils.o
+	$(LINKER) $(TESTCFLAGS) Plane.o PlaneTest.cpp -o planetest $(TESTLDFLAGS)
+	$(LINKER) $(TESTCFLAGS) Utils.o UtilsTest.cpp -o UtilsTest $(TESTLDFLAGS)
 	./planetest
+	./UtilsTest
---
+++ UtilsTest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <utility>
+#include <set>
+
+#include "Utils.h"
+
+using namespace std;
+
+namespace {
+
+// The fixture for testing class Foo.
+class UtilsTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  UtilsTest() {
+    // You can do set-up work for each test here.
+  }
+
+  virtual ~UtilsTest() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+// Tests that the Foo::Bar() method does Abc.
+TEST_F(UtilsTest, checkEquals) {
+  EXPECT_EQ(true, Utils::isEqualDouble(0.3, 0.1 + 0.2));
+}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
